af557e8b74ba6f942cad3ad82ed84e4df1d7049a
tulip3659 <tulip3659@yahoo.com>
2018-12-05 18:28:36 -0800

    Added practice_abstract_factory_2 with a config file as input

---
+++ practice/practice_abstract_factory_with_config.py
@@ -0,0 +1,35 @@
+"""
+In this practice I will use a config dict to pass the algorithm type to an abstract factory and "train".
+"""
+
+class Abstract_factory():
+    def __init__(self, config_dict):
+        # I am using globals()["TRPO"] to create a class from a string. There may be a better way
+        self._factory = globals()[config_dict["Algorithm"]]
+
+        print("Abstract factory created for a {} algorithm.".format(config_dict['Algorithm']))
+    def train(self):
+        concrete_object = self._factory()
+        return concrete_object.show_result()
+
+class SAC(object):
+    def __init__(self):
+        self.result = 'SAC trained !'
+    def show_result(self):
+        return self.result
+
+class TRPO(object):
+    def __init__(self):
+        self.result = 'TRPO trained !'
+    def show_result(self):
+        return self.result
+
+if __name__ == "__main__":
+    config_dict = dict(Algorithm = "TRPO", Type = "On policy")
+    test = Abstract_factory(config_dict)
+    print(test.train())
+
+    # Now, I keep everything untouched, just update the config_dict
+    config_dict = dict(Algorithm = "SAC", Type = "On policy")
+    test = Abstract_factory(config_dict)
+    print(test.train())
