2d21964870fc7873a56b0d008e2ea2f567af6c59
tulip3659 <tulip3659@yahoo.com>
2018-12-12 18:31:13 -0800

    Added practice_pool and minor changes to practice_queue

--- practice/practice_queue.py
+++ practice/practice_queue.py
@@ -6,11 +6,32 @@ https://dbader.org/blog/queues-in-python
 
 import queue
 
-q = queue.Queue()
+# q = queue.Queue()
+#
+# q.put("Babak")
+# q.put(60)
+#
+# for _ in range(5):
+#     # It doesn't raise exception Empty in python 3.5
+#     print(q.get())
 
-q.put("Babak")
-q.put(60)
 
-for _ in range(5):
-    # It doesn't raise exception Empty in python 3.5
-    print(q.get())
+class Sample_Class():
+    """
+    Sample class to see if you can put objects in a queue
+    """
+    def __str__(self):
+        return "Object!"
+
+
+qq = queue.Queue()
+
+obj1 = Sample_Class()
+obj2 = Sample_Class()
+
+qq.put(obj1)
+qq.put(obj2)
+print(qq.qsize())
+print(qq.get())
+print(qq.qsize())
+
